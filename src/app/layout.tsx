86737b345b597ada78289d71dbe4a33ee3528522
KristinaKuchnowetz <kuchnowetz.kristina@gmail.com>
2026-09-25 14:58:04 +0300

    fix: TS types fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import LayoutProps from "";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,7 +18,11 @@ export const metadata: Metadata = {
   description: "Upload a CSV and ask questions in plain English.",
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html
       lang="en"
